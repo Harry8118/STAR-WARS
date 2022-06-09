f8a67f7ff94c26aa902960eeac40a29b47d5d8cd
Harry <harutsargsyan46@gmail.com>
2022-06-09 18:12:55 +0400

    last commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-
+import bgImage from "./image/bgImage.jpeg";
 import "./App.css";
 import Moviepage from "./component/Moviepage";
 
--- src/component/Moviepage.jsx
+++ src/component/Moviepage.jsx
@@ -1,26 +1,46 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
+import { table } from "react-bootstrap";
 
 const baseURL = "https://swapi.dev/api/films/1/";
 
 const Moviepage = () => {
-  const [info, setInfo] = useState("");
+  const [info, setInfo] = useState({});
+  const [links, setLinks] = useState([]);
 
   useEffect(() => {
-    axios.get(`${baseURL}`).then((response) => {
+    axios.get(baseURL).then((response) => {
       setInfo(response.data);
+      let newLinks = [...links];
+      response.data.starships.map((item) => {
+        newLinks.push({ link: item, info: {} });
+      });
+      setLinks(newLinks);
     });
   }, [setInfo]);
 
+  const handleClickLink = (link, i) => {
+    axios.get(link).then((response) => {
+      let getInfo = [...links];
+      console.log(getInfo);
+      getInfo[i].info.name = response.data.name;
+      getInfo[i].info.model = response.data.model;
+      getInfo[i].info.passengers = response.data.passengers;
+      setLinks(getInfo);
+    });
+  };
+
   return (
     <div>
-      <h1>{info.title}</h1>
-      <p>{info.opening_crawl}</p>
-      <ul>
-        {info.starships?.map((el, i) => {
+      <h1 className="pagetitle">{info.title}</h1>
+      <p className="description">{info.opening_crawl}</p>
+      <ul className="starshiplist">
+        {links.map((el, i) => {
           return (
-            <li key={i}>
-              <a href={`${el}`}>{el}</a>
+            <li key={i} onClick={() => handleClickLink(el.link, i)}>
+              Starship {i + 1} {el.link} {el.info.name}
+              {el.info.model}
+              {el.info.passengers}
             </li>
           );
         })}
