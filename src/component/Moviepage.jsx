67230091f7964f80f6163485438f8746830f40e3
Harry <harutsargsyan46@gmail.com>
2022-06-11 17:16:46 +0400

    change info list and all stile. final commit

--- src/component/Moviepage.jsx
+++ src/component/Moviepage.jsx
@@ -1,7 +1,7 @@
 import React, { useEffect, useState } from "react";
 import axios from "axios";
-import { table } from "react-bootstrap";
-
+import bgImage from "../image/bgImage.jpeg";
+import logo from "../image/logo.png";
 const baseURL = "https://swapi.dev/api/films/1/";
 
 const Moviepage = () => {
@@ -17,12 +17,11 @@ const Moviepage = () => {
       });
       setLinks(newLinks);
     });
-  }, [setInfo]);
+  }, [setLinks, setInfo]);
 
   const handleClickLink = (link, i) => {
     axios.get(link).then((response) => {
       let getInfo = [...links];
-      console.log(getInfo);
       getInfo[i].info.name = response.data.name;
       getInfo[i].info.model = response.data.model;
       getInfo[i].info.passengers = response.data.passengers;
@@ -31,21 +30,64 @@ const Moviepage = () => {
   };
 
   return (
-    <div>
-      <h1 className="pagetitle">{info.title}</h1>
-      <p className="description">{info.opening_crawl}</p>
-      <ul className="starshiplist">
-        {links.map((el, i) => {
-          return (
-            <li key={i} onClick={() => handleClickLink(el.link, i)}>
-              Starship {i + 1} {el.link} {el.info.name}
-              {el.info.model}
-              {el.info.passengers}
-            </li>
-          );
-        })}
-      </ul>
-    </div>
+    <>
+      <div className="main">
+        <img style={{ width: "40%" }} src={bgImage} alt="" />
+        <div className="information">
+          <img src={logo} alt="" className="logo" />
+          <h1 className="pagetitle">{info.title}</h1>
+          <p className="description">{info.opening_crawl}</p>
+          <div className="tablewrap">
+            <div className="tablecontent">
+              <span className="line"></span>
+              <div className="col">
+                <span className="coltitle">#Number</span>
+                {links.map((el, i) => (
+                  <span key={i}>Starship{i + 1}</span>
+                ))}
+              </div>
+              <div className="col">
+                <span className="line2"></span>
+                <span className="coltitle">Link</span>
+                {links.map((el, i) => (
+                  <span key={i}>{el.link}</span>
+                ))}
+              </div>
+              <div className="col">
+                <span className="line2"></span>
+                <span className="coltitle">name</span>
+                {links.map((el, i) => {
+                  return <span key={i}>{el.info.name}</span>;
+                })}
+              </div>
+              <div className="col">
+                <span className="line2"></span>
+                <span className="coltitle">Model</span>
+                {links.map((el, i) => {
+                  return <span key={i}>{el.info.model}</span>;
+                })}
+              </div>
+              <div className="col">
+                <span className="line2"></span>
+                <span className="coltitle">Passengers</span>
+                {links.map((el, i) => {
+                  return <span key={i}>{el.info.passengers}</span>;
+                })}
+              </div>
+              <div className="col buttoncol">
+                <span className="line2"></span>
+                <span className="coltitle">Action</span>
+                {links.map((el, i) => (
+                  <button key={i} onClick={() => handleClickLink(el.link, i)}>
+                    get info
+                  </button>
+                ))}
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
   );
 };
 
